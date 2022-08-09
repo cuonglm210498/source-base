0e19845af3a566d6d5d3f56351d8b2f77b0ae29f
cuonglm18 <cuonglm18@viettelsoftware.vn>
2022-08-09 11:56:03 +0700

    update class DateTimeCommon and using in UserResponse

---
+++ src/main/java/com/lecuong/sourcebase/common/DateTimeCommon.java
@@ -0,0 +1,26 @@
+package com.lecuong.sourcebase.common;
+
+/**
+ * @author CuongLM18
+ * @created 09/08/2022 - 11:52 AM
+ * @project source-base
+ */
+public class DateTimeCommon {
+
+    public static class DATE_TIME_FORMAT {
+        public static final String YYYY_MM_DD = "yyyy-MM-dd";
+        public static final String YYYY_MM_DD_HYPHEN = "yyyy-MM-dd HH:mm:ss";
+        public static final String DD_MM_YYYY = "dd/MM/yyyy";
+        public static final String DD_MM_YYYY_HH = "dd/MM/yyyy HH:mm";
+        public static final String DD_MM_YYYY_HH_SS = "dd/MM/yyyy HH:mm:ss";
+        public static final String TIME_ZONE = "Asia/Ho_Chi_Minh";
+        public static final String YY_MM = "yyMM";
+        public static final String YYYY_MM_DD_HH_MM_SS_SSS = "yyyyMMddHHmmssSSS";
+        public static final String YYYY_MM_DD_HH_MM_SS = "yyyyMMddHHmmss";
+        public static final String YY_MM_DD_HH_MM = "yyMMddHHmm";
+        public static final String ISO_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
+        public static final String DATE_FORMAT_YYYY_MM_DD = "yyyy-MM-dd";
+        public static final String DATE_FORMAT_DD_MM_YYYY = "dd/MM/yyyy";
+        public static final String DATE_FORMAT_YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+    }
+}
--- src/main/java/com/lecuong/sourcebase/modal/response/UserResponse.java
+++ src/main/java/com/lecuong/sourcebase/modal/response/UserResponse.java
@@ -1,5 +1,7 @@
 package com.lecuong.sourcebase.modal.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.lecuong.sourcebase.common.DateTimeCommon;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -15,7 +17,10 @@ public class UserResponse {
     private String phone;
     private String address;
     private String fullName;
+
+    @JsonFormat(pattern = DateTimeCommon.DATE_TIME_FORMAT.DD_MM_YYYY, timezone = DateTimeCommon.DATE_TIME_FORMAT.TIME_ZONE)
     private LocalDate dateOfBirth;
+
     private String avatar;
     private transient List<String> roleName;
 }
