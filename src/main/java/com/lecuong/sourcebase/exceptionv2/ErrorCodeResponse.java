5b56db95339255e35791517a016da3e47989760d
cuonglm210498 <lemanhcuong0498@gmail.com>
2023-09-25 18:46:52 +0700

    update exception v2

--- src/main/java/com/lecuong/sourcebase/exceptionv2/ErrorCodeResponse.java
+++ src/main/java/com/lecuong/sourcebase/exceptionv2/ErrorCodeResponse.java
@@ -1,7 +1,20 @@
 package com.lecuong.sourcebase.exceptionv2;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
 /**
  * @author CuongLM
  * @created 25/09/2023 - 6:38 PM
  * @project source-base
- */public class ErrorCodeResponse {
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorCodeResponse {
+
+    private String code;
+    private String message;
+    private HttpStatus status;
 }
